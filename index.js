ac914b913feb160f7140331025618a73cb975d62
Brian Clozel <bclozel@gopivotal.com>
2014-04-08 11:55:45 +0200

    New 0.2.0 release
    * build against Gulp 3.6.0
    * upgrade and fix dependencies
    * fix relative path issue #5
    * log trace in plugin errors for cram
    
    Fixes #5

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var cram = require('cram');
 var cramSrc = function(filename, opts) {
 
     var PLUGIN_NAME = "gulp-cram";
+    var CRAM_OUTPUT_FILE = ".gulpcram.js";
 
     opts.grok = filename;
 
@@ -24,8 +25,8 @@ var cramSrc = function(filename, opts) {
 
         // write cram results to a temp hidden file
         var cwd = process.cwd();
-        var cramOutput = path.resolve(cwd, ".gulpcram.js");
-        opts.output = cramOutput;
+        var cramOutput = path.resolve(cwd, CRAM_OUTPUT_FILE);
+        opts.output = CRAM_OUTPUT_FILE;
 
         // this is the output file name that will be streamed
         var outputPath = path.resolve(cwd, output);
@@ -37,8 +38,8 @@ var cramSrc = function(filename, opts) {
         var promise = when(cram(opts));
 
         // throw a gulp plugin error if cram can't complete its task
-        var cramError = function(ex) {
-            throw new PluginError(PLUGIN_NAME, "Cram error", ex);
+        var cramError = function() {
+            throw new PluginError(PLUGIN_NAME, "Cram error", {showStack: true});
         };
 
         // when finished, read cram temp output and push it in the result stream as a vinyl file
